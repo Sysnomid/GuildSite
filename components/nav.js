85765711664258d76248514481a292261461cefe
Sysnomid <sam@sysnomid.com>
2020-10-24 19:53:20 -0400

    test

--- components/nav.js
+++ components/nav.js
@@ -10,6 +10,7 @@ export default function Navbar({ fixed }) {
         <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"> 
             
             <a className="text-xl leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap text-white" href="/"> The Developers Guild</a>
+           
             <button
               className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
