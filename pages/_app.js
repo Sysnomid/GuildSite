2c5c90f3b84e5c762741acbcc9c1532d8f453557
Sysnomid <sam@sysnomid.com>
2020-10-25 13:45:36 -0400

    Change FOnt

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ function App({ Component, pageProps }) {
     <>
   <Head>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fork-awesome@1.1.7/css/fork-awesome.min.css" integrity="sha256-gsmEoJAws/Kd3CjuOQzLie5Q3yshhvmo7YNtBG7aaEY=" crossorigin="anonymous"></link>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@300;400&display=swap" rel="stylesheet" /> 
   </Head>
     <title>
       The Developers' Guild Community
