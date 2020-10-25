77bcdc6e5f1b2e71d0cd864eb4f6b89ef9659c78
Sysnomid <sam@sysnomid.com>
2020-10-25 13:22:45 -0400

    title

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,9 @@ function App({ Component, pageProps }) {
   <Head>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fork-awesome@1.1.7/css/fork-awesome.min.css" integrity="sha256-gsmEoJAws/Kd3CjuOQzLie5Q3yshhvmo7YNtBG7aaEY=" crossorigin="anonymous"></link>
   </Head>
-
+    <title>
+      The Developers' Guild Community
+    </title>
   <div className="flex flex-col h-screen justify-between">
       <Nav />
         <Component {...pageProps} />
