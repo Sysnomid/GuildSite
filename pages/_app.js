de4927f42e454a65073df675bf624149b90cb32d
Sysnomid <sam@sysnomid.com>
2020-10-25 14:24:59 -0400

    Meta Tags 2

--- pages/_app.js
+++ pages/_app.js
@@ -9,9 +9,26 @@ function App({ Component, pageProps }) {
     <Head>
       <link href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap" rel="stylesheet" /> 
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fork-awesome@1.1.7/css/fork-awesome.min.css" integrity="sha256-gsmEoJAws/Kd3CjuOQzLie5Q3yshhvmo7YNtBG7aaEY=" crossorigin="anonymous"></link>
-      <meta name="Image" content="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
-      <meta name="Title" content="The Developers Guild Community" />
-      <meta name="Description" content="Learn Python, JS, SQL, C#, C/C++ and more!" />
+      
+      <meta name="image" content="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
+      <meta name="title" content="The Developers Guild Community" />
+      <meta name="description" content="Learn Python, JS, SQL, C#, C/C++ and more!" />
+
+      <meta name="og:image" 
+        content="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" 
+        key="ogimage"
+      />
+
+      <meta name="og:title" 
+        content="The Developers Guild Community" 
+        key="ogtitle"
+      />
+      <meta 
+        name="og:description" 
+        content="Learn Python, JS, SQL, C#, C/C++ and more!" 
+        key="ogdesc"  
+      />
+
     </Head>
       <title>
         The Developers' Guild Community
