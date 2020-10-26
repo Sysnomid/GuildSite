2bb006e83493c6756dcd2b2ba8eae18cee5eab28
Sysnomid <sam@sysnomid.com>
2020-10-25 22:04:43 -0400

    Update Footer

--- components/footer.js
+++ components/footer.js
@@ -7,20 +7,27 @@ export default function Footer(){
             <hr width="0"/>
             <footer className="h-auto bg-gray-800 bg-opacity-30">
                 <br />
-                <div className="txt-marg flex justify-center items-center">
+                <div className="txt-marg flex justify-center items-center mt-2 mb-2 text-1xl">
                 &copy; Copyright The Developers Guild { Year }
                
-                <div className="flex gap-3">
+                <div className="flex gap-3 mr-6 ml-6">
                     <br />
                     
                     <a href="/invite">
-                        <i className="fa fa-discord fa-1x" aria-hidden="true" />
+                        <i className="fa fa-discord fa-2x" aria-hidden="true" />
                     </a>
                 
                     <br />
                     
-                    <a href="https://github.com/The-Developers-Guild">
-                        <i className="fa fa-github fa-1x" aria-hidden="true" />
+                    <a href="/github">
+                        <i className="fa fa-github fa-2x" aria-hidden="true" />
+                    </a>
+
+                    <br />
+
+                                        
+                    <a href="/contact">
+                        <i className="fa fa-address-book-o fa-2x" aria-hidden="true" />
                     </a>
 
                 </div>
