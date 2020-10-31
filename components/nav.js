4f4fe942fe36afea9795dfcdc5f5560d543d2695
Sysnomid <sam@sysnomid.com>
2020-10-31 07:59:48 -0400

    Aria Label and New Buttons

--- components/nav.js
+++ components/nav.js
@@ -56,6 +56,7 @@ export default class Nav extends React.Component {
 							onClick={this.toggleNavMenu}
 							type="button"
 							className="text-gray-500 hover:text-white focus:text-white focus:outline-none"
+							aria-label="Navbar Menu"
 						>
 							<svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
 								{menuIcon}
--- pages/index.js
+++ pages/index.js
@@ -34,21 +34,27 @@ function HomeHeader(){
   return (
     <>  
         <div id="sec1" className="bg-blue-900">
-        <br />
-        <br />
-        <h1 className="text-base text-5xl text-center break-words">
-        Need Help With
-             <h1 className="gradient h-24 bg-gradient-to-r from-teal-300 to-blue-500 break-words">
-                <TypedString />?
-         </h1>       
-            <a href="https://discord.gg/xqUN8KY">  
-              <i class="fa fa-discord" />
-              <br />
-              <button className="text-xl break-word bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Discord Invite</button>
+          <br />
+          <br />
+
+          <h1 className="text-base text-5xl text-center break-words font-bold">
+              Need Help With
+              <h1 className="font-bold gradient h-24 bg-gradient-to-r from-teal-300 to-blue-500 break-words">
+              <TypedString />?
+          </h1>       
+
+            <a href="https://discord.gg/xqUN8KY" className="mb-20">  
+              <button className="text-xl font-bold break-word bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+                <i class="fa fa-discord fa-2x mb-2" /> <br /> 
+                  <strong>Discord Invite</strong>
+              </button>
             </a>
-        <br />
-        <br />
-        </h1>
+
+          <br />
+          <br />
+
+          </h1>
+          
         </div>
     </>
   )
@@ -63,12 +69,12 @@ function Description(){
        
         <h1 class="text-4xl text-teal-500 mb-10"><b>Join The Developers Guild!</b></h1>     
 
-       <div className="txt-marg text-base text-2xl">     
+       <div className="txt-marg text-base">     
  
-              <div id="statement" className="flex-col justify-center flex gap-10">
+              <div id="statement" className="flex-col justify-center flex">
                 
                   <div>
-                  The Developer's Guild is a coalition of about <strong>1000</strong> very active student and professional developers. 
+                  The Developer's Guild is a community of about <strong className="font-bold">1000</strong> very active student and professional developers. 
                   </div>
 
                   <div>
@@ -84,6 +90,17 @@ function Description(){
                   </div>
                   
               </div>
+
+              <style jsx>
+                {`
+                
+                  #statement {
+                    grid-gap: 2.5rem;
+                    font-size: 1.6rem;
+                  }
+
+                `}
+              </style>
       </div>
 
 
