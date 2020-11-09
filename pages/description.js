72d385dc163924bb61e95d17413405f8850db3b1
Sysnomid <52793419+Sysnomid@users.noreply.github.com>
2020-11-09 16:40:15 -0500

    Update description.js

--- pages/description.js
+++ pages/description.js
@@ -4,13 +4,13 @@ export default function Description() {
         <div id="section-2" className="bg-gray-900 text-center pt-10 pb-20 break-words">        
           
           <h1 class="text-4xl text-teal-500 pb-10 mr-1 ml-1">
-            <b>Join The Developers Guild!</b>
+            <b>Join The Devs' Guild!</b>
           </h1>     
           
           <div id="description" className="txt-marg text-base">     
                 <div id="statement" className="flex-col justify-center flex text-2xl">
                     <div className="pb-6">
-                    The Developer's Guild is a community of about <strong className="font-bold">1000</strong> very active student and professional developers. 
+                    The Devs' Guild is a community of about <strong className="font-bold">1000</strong> very active student and professional developers. 
                     </div>
   
                     <div className="pb-6">
